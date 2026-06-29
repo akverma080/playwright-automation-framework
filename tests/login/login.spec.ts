476c3da7c5b94928f64cd93a03a038447379e1c4
Amit Kumar <akverma080@gmail.com>
2026-06-29 21:48:22 +0530

    Modify the test

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,17 +1,20 @@
 import {test, expect} from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { BasePage } from '../../pages/BasePage';
+const INVALID_LOGIN_MESSAGE = 'Invalid username or password.';
 
-test.describe.skip('Login Tests', () => {   
+test.describe('Login Tests', () => {
 
-test('should display error message for invalid credentials', async ({ page }) => {
+  test('should display an error message for invalid credentials', async ({ page }) => {
+  
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.openLogin();
+    await loginPage.loginWithWrongCredentials();
 
-  const loginPage = new LoginPage(page);
-  await loginPage.openLogin();
-// await loginPage.loginWithWrongCredentials();
-
-// await expect(loginPage.errorMessage)
-//   .toHaveText('Invalid username or password.');
- });
+    // Assert
+    await expect(loginPage.errorMessage)
+      .toHaveText('Invalid username or password.');
+  });
 
 });
--- tests/product-details.spec.ts
+++ tests/product-details.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { HomePage } from '../pages/HomePage'
 import { NavigationComponent } from '../components/NavigationComponent';
 import { ProductCategory } from '../pages/ProductCategory';
@@ -6,28 +6,24 @@ import { ProductCategory } from '../pages/ProductCategory';
 test.describe('Home Page', () => {
 
 
-  test('should display the correct title', async ({page}) =>{
-
+  test('should display the correct title', async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.navigateToHome();
     const pageTitle = await page.title();
     expect(pageTitle).toContain('Scentelio');
-
-
   });
 
-  
-  test('should display for men link', async ({page}) =>{
+
+  test('should display for men link', async ({ page }) => {
 
     const homePage = new HomePage(page);
     await homePage.navigateToHome();
     const nav = new NavigationComponent(page);
 
-await nav.clickMenuLinkByName('For Men');
+    await nav.clickMenuLinkByName('For Men');
     const productCategoryTitle = new ProductCategory(page);
     const title = await productCategoryTitle.getPageTitle();
     expect(title).toContain('For Men');
-
 
   });
 
